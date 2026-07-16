b6a79c24a230e2e2df42291de7fadc5d6c931bf5
melanieemis-ops <melanieemis@icloud.com>
2026-07-16 19:10:14 +0200

    zzzzz

--- src/routes/magazin.tsx
+++ src/routes/magazin.tsx
@@ -71,9 +71,7 @@ function MagazinPage() {
 
                 <div className="relative overflow-hidden rounded-[1.4rem] border border-border/70 bg-card p-2 shadow-[0_24px_70px_-28px_rgba(15,23,42,0.45)]">
                   <img
-                    src="/icons/cover.png
-                    alt="Cover des steuerstoff Magazins – Ausgabe 01/2026"
-                    className="block h-auto w-full rounded-[1rem] object-contain"
+                    src= "/icons/cover.png"
                     loading="eager"
                     decoding="async"
                   />

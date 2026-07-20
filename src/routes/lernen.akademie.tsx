93fb1e6ad3a7b991c45c9dafd2f947127f52826f
Melanie Misakian <melanie@steuerstoff.de>
2026-07-20 19:51:22 +0200

    ol

--- src/routes/lernen.akademie.tsx
+++ src/routes/lernen.akademie.tsx
@@ -400,6 +400,7 @@ function ExamTrainingCard() {
   return (
     <Link
       to="/lernen/akademie/klausuren"
+      search={{}}
       className="group flex min-h-52 flex-col rounded-[1.75rem] border border-border/70 bg-card p-5 shadow-sm transition-all hover:-translate-y-1 hover:shadow-md sm:p-6"
     >
       <div className="flex items-start justify-between gap-3">

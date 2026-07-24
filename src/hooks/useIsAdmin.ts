7d491646b98ea81595f5590392840733564e9b32
Steuerstoff <melanieemis@icloud.com>
2026-07-24 18:27:01 +0200

    Register Eigenverbrauch KB category article

--- src/hooks/useIsAdmin.ts
+++ src/hooks/useIsAdmin.ts
@@ -4,6 +4,7 @@ import "@/lib/knowledgeBaseExtensions/lohnsteuer-aufmerksamkeiten";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-auslandsaufenthalt";
 import "@/lib/knowledgeBaseExtensions/sozialversicherung-betriebspruefung";
 import "@/lib/knowledgeBaseExtensions/kfz-dienstwagen-1-prozent";
+import "@/lib/knowledgeBaseExtensions/eigenverbrauch-unentgeltliche-wertabgaben-lieferungen";
 
 const KEY = "steuerstoff:admin";
 

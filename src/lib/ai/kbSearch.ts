1e0bf112b0161be60e115d627803ae7aeccda7f5
Steuerstoff <melanieemis@icloud.com>
2026-07-24 17:50:23 +0200

    Include social insurance audit entry in AI knowledge search

--- src/lib/ai/kbSearch.ts
+++ src/lib/ai/kbSearch.ts
@@ -6,6 +6,7 @@
 import "@/lib/knowledgeBaseExtensions/aufbewahrungspflichten-ao";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-aufmerksamkeiten";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-auslandsaufenthalt";
+import "@/lib/knowledgeBaseExtensions/sozialversicherung-betriebspruefung";
 import { KNOWLEDGE_BASE, kbKeywordsToRegExp, type KBEntry } from "@/lib/knowledgeBase";
 
 import { INTERNAL_KNOWLEDGE_BASE } from "@/lib/expertSystem/internalKnowledge";

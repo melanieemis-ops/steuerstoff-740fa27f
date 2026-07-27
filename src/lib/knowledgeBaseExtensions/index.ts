b0f5a4fa88bdcf9a5a6bf4dbc80aade2bec0ef11
Steuerstoff <melanieemis@icloud.com>
2026-07-27 20:48:17 +0200

    feat(kb): NPO-BFH-Rechtsprechungsbeitrag registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -42,6 +42,7 @@ import "@/lib/knowledgeBaseExtensions/lohnsteuer-elektronische-lohnsteuerbeschei
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-faelligkeit-lohnsteuer-sozialversicherungsbeitraege";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-lohn-und-gehaltsabrechnung";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-lohnsteuerbescheinigung-erstellung-korrektur-inhalt";
+import "@/lib/knowledgeBaseExtensions/npo-gemeinnuetzigkeit-bfh-demokratie-verfassungsschutz-zweckbetrieb-krankenhaus";
 import "@/lib/knowledgeBaseExtensions/npo-gemeinnuetzigkeit-grundlagen-rechtsprechung-gesetzgebung-2026";
 import "@/lib/knowledgeBaseExtensions/personengesellschaften-bilanzierung-beteiligungen-idw-rs-fab-18";
 import "@/lib/knowledgeBaseExtensions/personengesellschaften-grundlagen-steuerliche-besonderheiten";

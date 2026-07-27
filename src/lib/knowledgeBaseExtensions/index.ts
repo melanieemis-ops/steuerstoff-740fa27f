3ba557dc06f8bb3712856750c1e89c6c3923dea4
Steuerstoff <melanieemis@icloud.com>
2026-07-27 13:48:08 +0200

    feat(kb): Gewerbesteueranrechnung registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -25,6 +25,7 @@ import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-festsetzungsverjaehrung";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-gesetzliche-erbfolge";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-steuerstrafrechtliche-risiken-ehegatten";
 import "@/lib/knowledgeBaseExtensions/ertragsteuer-anschaffungsnaher-aufwand-6-abs-1-nr-1a-estg";
+import "@/lib/knowledgeBaseExtensions/gewerbesteuer-anrechnung-steuerermaessigung-35-estg";
 import "@/lib/knowledgeBaseExtensions/gewerbesteuer-berechnung-rueckstellung";
 import "@/lib/knowledgeBaseExtensions/grunderwerbsteuer-aktuelle-entwicklung-2026-rechtsprechung";
 import "@/lib/knowledgeBaseExtensions/jahresabschluss-geleistete-erhaltene-anzahlungen";

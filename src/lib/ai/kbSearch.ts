95e2a3b72d40afa3e14272fd6de303027f5a2668
Steuerstoff <melanieemis@icloud.com>
2026-07-27 20:49:04 +0200

    feat(ai): NPO-BFH-Rechtsprechung für KB-Suche registrieren

--- src/lib/ai/kbSearch.ts
+++ src/lib/ai/kbSearch.ts
@@ -52,6 +52,7 @@ import "@/lib/knowledgeBaseExtensions/lohnsteuer-aussenpruefung";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-festsetzungsverjaehrung";
 import "@/lib/knowledgeBaseExtensions/ertragsteuer-anschaffungsnaher-aufwand-6-abs-1-nr-1a-estg";
 import "@/lib/knowledgeBaseExtensions/bilanzierung-grundlagen-steuerlicher-bilanzenzusammenhang";
+import "@/lib/knowledgeBaseExtensions/npo-gemeinnuetzigkeit-bfh-demokratie-verfassungsschutz-zweckbetrieb-krankenhaus";
 import { KNOWLEDGE_BASE, kbKeywordsToRegExp, type KBEntry } from "@/lib/knowledgeBase";
 
 import { INTERNAL_KNOWLEDGE_BASE } from "@/lib/expertSystem/internalKnowledge";

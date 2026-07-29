8ba426be4919b9dcc0fcf90427117ef41e1f5001
Steuerstoff <melanieemis@icloud.com>
2026-07-30 01:23:47 +0200

    feat(kb): register § 4a EStG article

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -26,6 +26,7 @@ import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-2-estg-grundlagen
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-3-estg-steuerfreie-einnahmen";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-3b-estg-sonntags-feiertags-nachtzuschlaege";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-3c-estg-anteilige-abzuege";
+import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-4a-estg-gewinnermittlungszeitraum-wirtschaftsjahr";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-teilentgeltliche-grundstuecksuebertragung-23-estg";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-vorsorgepauschale-ab-2026";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-bewertung-betriebsvermoegen-vereinfachtes-ertragswertverfahren";

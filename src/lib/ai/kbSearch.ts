09b4bd862bc25353a4f3360191b490338e830e8b
Steuerstoff <melanieemis@icloud.com>
2026-07-30 00:54:35 +0200

    KB-Suche um § 3 EStG ergänzen

--- src/lib/ai/kbSearch.ts
+++ src/lib/ai/kbSearch.ts
@@ -23,6 +23,7 @@ import "@/lib/knowledgeBaseExtensions/sozialversicherung-pruefpflichten-steuerbe
 import "@/lib/knowledgeBaseExtensions/sozialversicherung-unfallversicherung-homeoffice-mobiles-arbeiten-mittagspause-2026";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-entfernungspauschale-2026";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-kinderbetreuungskosten-getrennte-eltern-haushaltszugehoerigkeit";
+import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-3-estg-steuerfreie-einnahmen";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-teilentgeltliche-grundstuecksuebertragung-23-estg";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-vorsorgepauschale-ab-2026";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-grundstueckseigentuemer-update-2025-2026-paragraf-21";

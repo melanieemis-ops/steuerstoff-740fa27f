e7b6a3360bcbbf611a6efdbf8ee0b0c7e7b55de9
Steuerstoff <melanieemis@icloud.com>
2026-07-30 01:49:20 +0200

    Register § 6b EStG knowledge base article

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -29,6 +29,7 @@ import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-3c-estg-anteilige
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-4a-estg-gewinnermittlungszeitraum-wirtschaftsjahr";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-5-estg-gewinn-bei-kaufleuten";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6-estg-bewertung";
+import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6b-estg-uebertragung-stiller-reserven";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-teilentgeltliche-grundstuecksuebertragung-23-estg";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-vorsorgepauschale-ab-2026";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-bewertung-betriebsvermoegen-vereinfachtes-ertragswertverfahren";

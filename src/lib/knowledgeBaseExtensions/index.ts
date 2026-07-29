fd83902b74d6a95d296e23a25a8d0a92942ef0be
Steuerstoff <melanieemis@icloud.com>
2026-07-29 20:27:27 +0200

    Arbeitnehmersparzulage im KB-Index registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -39,6 +39,7 @@ import "@/lib/knowledgeBaseExtensions/jahresabschluss-immaterielle-vermoegensgeg
 import "@/lib/knowledgeBaseExtensions/kfz-dienstwagen-1-prozent";
 import "@/lib/knowledgeBaseExtensions/koerperschaftsteuer-darlehen-betriebspruefung-8b-abs-3-kstg";
 import "@/lib/knowledgeBaseExtensions/koerperschaftsteuer-gmbh-grundlagen-steuerliche-besonderheiten";
+import "@/lib/knowledgeBaseExtensions/lohnsteuer-arbeitnehmersparzulage";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-aufmerksamkeiten";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-auslandsaufenthalt";
 import "@/lib/knowledgeBaseExtensions/lohnsteuer-aussenpruefung";

a6ab83023f33b3459cfe5651b7f8781751745052
Steuerstoff <melanieemis@icloud.com>
2026-07-29 22:04:41 +0200

    KB: Gebäude-AfaA registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -4,6 +4,7 @@
 import { KNOWLEDGE_BASE } from "@/lib/knowledgeBase";
 import "@/lib/knowledgeBaseExtensions/abschreibung-afa-wertminderungen-hgb-estg-ifrs";
 import "@/lib/knowledgeBaseExtensions/abschreibung-ausserplanmaessige-wertminderung-afaa";
+import "@/lib/knowledgeBaseExtensions/abschreibung-gebaeude-aussergewoehnliche-abnutzung-afaa";
 import "@/lib/knowledgeBaseExtensions/abschreibung-gemischt-genutzte-gebaeude-afa";
 import "@/lib/knowledgeBaseExtensions/abschreibung-sonderabschreibungen-7a-7b-7g-estg";
 import "@/lib/knowledgeBaseExtensions/abschreibung-umlaufvermoegen-niederstwertprinzip";

47c5a48353f3369ba8722a1a6555ceef92b9d835
Steuerstoff <melanieemis@icloud.com>
2026-07-30 02:01:23 +0200

    KB-Index: § 7a EStG registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -30,6 +30,7 @@ import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-4a-estg-gewinnerm
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-5-estg-gewinn-bei-kaufleuten";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6-estg-bewertung";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6b-estg-uebertragung-stiller-reserven";
+import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-7a-estg-erhoehte-absetzungen-sonderabschreibungen";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-teilentgeltliche-grundstuecksuebertragung-23-estg";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-vorsorgepauschale-ab-2026";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-bewertung-betriebsvermoegen-vereinfachtes-ertragswertverfahren";

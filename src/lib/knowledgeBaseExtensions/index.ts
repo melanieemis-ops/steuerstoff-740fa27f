a3822ceebc6d1b77c2118b7301c56a021dece92b
Steuerstoff <melanieemis@icloud.com>
2026-07-30 02:11:31 +0200

    § 7g EStG in Wissensdatenbank registrieren

--- src/lib/knowledgeBaseExtensions/index.ts
+++ src/lib/knowledgeBaseExtensions/index.ts
@@ -32,6 +32,7 @@ import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6-estg-bewertung"
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-6b-estg-uebertragung-stiller-reserven";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-7a-estg-erhoehte-absetzungen-sonderabschreibungen";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-7b-estg-sonderabschreibung-mietwohnungsneubau";
+import "@/lib/knowledgeBaseExtensions/einkommensteuer-paragraf-7g-estg-investitionsabzugsbetrag-sonderabschreibung";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-teilentgeltliche-grundstuecksuebertragung-23-estg";
 import "@/lib/knowledgeBaseExtensions/einkommensteuer-vorsorgepauschale-ab-2026";
 import "@/lib/knowledgeBaseExtensions/erbschaftsteuer-bewertung-betriebsvermoegen-vereinfachtes-ertragswertverfahren";
